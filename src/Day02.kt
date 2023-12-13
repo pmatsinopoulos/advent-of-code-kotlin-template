8a4177ae79dd11eabbfae804226e6a3803421a59
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2023-12-13 10:03:26 +0200

    Day02 - Part 2 is ready

--- src/Day02.kt
+++ src/Day02.kt
@@ -12,7 +12,7 @@ enum class Color {
 }
 
 data class SetOfCubes(
-    val numberForColor: MutableMap<Color, Int> = mutableMapOf() // e.g. RED -> 4, GREEN -> 5, BLUE -> 6
+    val numbersForColors: MutableMap<Color, Int> = mutableMapOf() // e.g. RED -> 4, GREEN -> 5, BLUE -> 6
 )
 
 data class Game(
@@ -30,7 +30,7 @@ fun buildSetOfCubes(input: String): SetOfCubes {
     val numbersForColors = input.split(",").map { it.trim() }
     val setOfCubes = numbersForColors.fold(SetOfCubes()) { acc, numberForColor ->
         val (number, color) = numberForColor.split(" ").map { it.trim() }
-        acc.numberForColor[Color.valueOf(color.uppercase())] = number.toInt()
+        acc.numbersForColors[Color.valueOf(color.uppercase())] = number.toInt()
         acc
     }
     return setOfCubes
@@ -46,18 +46,30 @@ fun buildGame(input: String): Game {
 }
 
 fun possibleSet(set: SetOfCubes, input: Input): Boolean {
-    return set.numberForColor.getOrDefault(Color.RED, 0) <= input.red &&
-            set.numberForColor.getOrDefault(Color.GREEN, 0) <= input.green &&
-            set.numberForColor.getOrDefault(Color.BLUE, 0) <= input.blue
+    return set.numbersForColors.getOrDefault(Color.RED, 0) <= input.red &&
+            set.numbersForColors.getOrDefault(Color.GREEN, 0) <= input.green &&
+            set.numbersForColors.getOrDefault(Color.BLUE, 0) <= input.blue
 }
 
 fun possibleGame(game: Game, input: Input): Boolean {
     return game.sets.all { set -> possibleSet(set, input) }
 }
 
+fun powerOfColor(set: SetOfCubes, color: Color): Int {
+    return set.numbersForColors.getOrDefault(color, 1)
+}
+
+fun maxPowerOfColor(game: Game, color: Color): Int {
+    return game.sets.map { set -> powerOfColor(set, color) }.max()
+}
+
+fun powerOfGame(game: Game): Int {
+    return maxPowerOfColor(game, Color.RED) * maxPowerOfColor(game, Color.GREEN) * maxPowerOfColor(game, Color.BLUE)
+}
+
 fun main() {
     val input = Input(red = 12, green = 13, blue = 14)
-    val result = readInput("Day02").sumOf { inputGame ->
+    val resultPart1 = readInput("Day02").sumOf { inputGame ->
         val game = buildGame(inputGame)
         if (possibleGame(game, input)) {
             game.index
@@ -66,5 +78,12 @@ fun main() {
         }
     }
 
-    println("result = $result") // Should be 2447 for the first part
+    println("part 1 result = $resultPart1") // Should be 2447
+
+    val resultPart2 = readInput("Day02").sumOf { inputGame ->
+        val game = buildGame(inputGame)
+        powerOfGame(game)
+    }
+
+    print("part 2 result = $resultPart2") // Should be 56322
 }
