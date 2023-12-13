2936cd6e42bb460b722eb29797ab44470ac3d626
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2023-12-13 10:13:59 +0200

    Day 02 - Made the solution more object oriented

--- src/Day02.kt
+++ src/Day02.kt
@@ -13,12 +13,49 @@ enum class Color {
 
 data class SetOfCubes(
     val numbersForColors: MutableMap<Color, Int> = mutableMapOf() // e.g. RED -> 4, GREEN -> 5, BLUE -> 6
-)
+) {
+    fun possible(input: Input): Boolean = numbersForColors.getOrDefault(Color.RED, 0) <= input.red &&
+            numbersForColors.getOrDefault(Color.GREEN, 0) <= input.green &&
+            numbersForColors.getOrDefault(Color.BLUE, 0) <= input.blue
+
+    fun powerOfColor(color: Color): Int = numbersForColors.getOrDefault(color, 1)
+
+    companion object {
+        fun build(input: String): SetOfCubes {
+            val numbersForColors = input.split(",").map { it.trim() }
+            val setOfCubes = numbersForColors.fold(SetOfCubes()) { acc, numberForColor ->
+                val (number, color) = numberForColor.split(" ").map { it.trim() }
+                acc.numbersForColors[Color.valueOf(color.uppercase())] = number.toInt()
+                acc
+            }
+            return setOfCubes
+        }
+    }
+}
 
 data class Game(
     val index: Int,
     val sets: List<SetOfCubes>
-)
+) {
+    fun possible(input: Input): Boolean = sets.all { set -> set.possible(input) }
+
+    fun power(): Int =
+
+        maxPowerOfColor(Color.RED) * maxPowerOfColor(Color.GREEN) * maxPowerOfColor(Color.BLUE)
+
+    private fun maxPowerOfColor(color: Color): Int = sets.map { set -> set.powerOfColor(color) }.max()
+
+    companion object {
+        fun build(input: String): Game {
+            val (game, setsString) = input.split(":").map { it.trim() }
+            val gameIndex = game.replace("Game ", "").toInt()
+            val sets = setsString.split(";").map { set -> set.trim() }
+            val setsOfCubes = sets.map { s -> SetOfCubes.build(s) }
+
+            return Game(index = gameIndex, sets = setsOfCubes)
+        }
+    }
+}
 
 data class Input(
     val red: Int,
@@ -26,52 +63,11 @@ data class Input(
     val blue: Int
 )
 
-fun buildSetOfCubes(input: String): SetOfCubes {
-    val numbersForColors = input.split(",").map { it.trim() }
-    val setOfCubes = numbersForColors.fold(SetOfCubes()) { acc, numberForColor ->
-        val (number, color) = numberForColor.split(" ").map { it.trim() }
-        acc.numbersForColors[Color.valueOf(color.uppercase())] = number.toInt()
-        acc
-    }
-    return setOfCubes
-}
-
-fun buildGame(input: String): Game {
-    val (game, setsString) = input.split(":").map { it.trim() }
-    val gameIndex = game.replace("Game ", "").toInt()
-    val sets = setsString.split(";").map { set -> set.trim() }
-    val setsOfCubes = sets.map { s -> buildSetOfCubes(s) }
-
-    return Game(index = gameIndex, sets = setsOfCubes)
-}
-
-fun possibleSet(set: SetOfCubes, input: Input): Boolean {
-    return set.numbersForColors.getOrDefault(Color.RED, 0) <= input.red &&
-            set.numbersForColors.getOrDefault(Color.GREEN, 0) <= input.green &&
-            set.numbersForColors.getOrDefault(Color.BLUE, 0) <= input.blue
-}
-
-fun possibleGame(game: Game, input: Input): Boolean {
-    return game.sets.all { set -> possibleSet(set, input) }
-}
-
-fun powerOfColor(set: SetOfCubes, color: Color): Int {
-    return set.numbersForColors.getOrDefault(color, 1)
-}
-
-fun maxPowerOfColor(game: Game, color: Color): Int {
-    return game.sets.map { set -> powerOfColor(set, color) }.max()
-}
-
-fun powerOfGame(game: Game): Int {
-    return maxPowerOfColor(game, Color.RED) * maxPowerOfColor(game, Color.GREEN) * maxPowerOfColor(game, Color.BLUE)
-}
-
 fun main() {
     val input = Input(red = 12, green = 13, blue = 14)
     val resultPart1 = readInput("Day02").sumOf { inputGame ->
-        val game = buildGame(inputGame)
-        if (possibleGame(game, input)) {
+        val game = Game.build(inputGame)
+        if (game.possible(input)) {
             game.index
         } else {
             0
@@ -81,8 +77,8 @@ fun main() {
     println("part 1 result = $resultPart1") // Should be 2447
 
     val resultPart2 = readInput("Day02").sumOf { inputGame ->
-        val game = buildGame(inputGame)
-        powerOfGame(game)
+        val game = Game.build(inputGame)
+        game.power()
     }
 
     print("part 2 result = $resultPart2") // Should be 56322
